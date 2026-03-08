aed14dbf179ea4c8fe11f62505e8c9d62a943826
Exonical <brycemanglin@gmail.com>
2026-03-08 04:07:11 -0400

    Remove vendor directory from repository
    - Delete all vendored dependencies from vendor/
    - Dependencies should be managed via go.mod instead of vendoring

--- vendor/github.com/siderolabs/go-blockdevice/v2/block/utils.go
+++
@@ -1,9 +0,0 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-package block
-
-func isPowerOf2[T uint8 | uint16 | uint32 | uint64 | uint](num T) bool {
-	return (num != 0 && ((num & (num - 1)) == 0))
-}
